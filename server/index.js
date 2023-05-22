00cbdb11f57a00739c926155b812ee1c3c6a2982
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-05-22 15:14:00 -0400

    added if production and build path

--- server/index.js
+++ server/index.js
@@ -8,20 +8,29 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const emailjs = require('@emailjs/nodejs');
-require('dotenv').config({ path:__dirname+'/.env' });
+require('dotenv').config({ path: __dirname + '/.env' });
 
 app.post("/api/contact-form", (req, res) => {
     emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, req.body, { publicKey: process.env.PUBLIC_KEY, privateKey: process.env.PRIVATE_KEY })
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
     res.json({ message: "Message SENT!" });
 });
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.get('/api/resume', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/resume.pdf'))
+    res.sendFile(path.join(__dirname, 'public/resume.pdf'))
 );
 
 app.listen(PORT, () => {
